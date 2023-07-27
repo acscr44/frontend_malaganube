9046403bdc3fc74f0912f0ee72641b964d643d5f
Alejandro <acastillo44@gmail.com>
2023-07-27 14:17:48 +0200

    app-routing.modules.ts solución al Refresh

--- Alumno/my-app/src/app/app-routing.module.ts
+++ Alumno/my-app/src/app/app-routing.module.ts
@@ -9,25 +9,28 @@ import { RestaurantesComponent } from './components/restaurantes/restaurantes.co
 import { FormularioRestauranteComponent } from './components/formulario-restaurante/formulario-restaurante.component';
 import { RestaurantesnpagComponent } from './components/restaurantesnpag/restaurantesnpag.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 // en este array de rutas, tengo que tener una ruta path
 // asociado al componente.
 const routes: Routes = [
-  { path: 'dni', component: DniComponent },
-  { path: 'adivina', component: AdivinaComponent },
-  { path: 'perros', component: PerroComponent },
-  { path: 'basicos-ng', component: BasicosNgComponent },
-  { path: 'API-norris', component: APINorrisComponent },
-  { path: 'restaurantes', component: RestaurantesComponent },
-  { path: 'restaurantes/formularioRestaurante', component: FormularioRestauranteComponent },
-  { path: 'restaurantesnpag', component: RestaurantesnpagComponent },
-  { path: 'busqueda', component: BusquedaComponent }
-  
-
+  { path: "dni", component: DniComponent },
+  { path: "adivina", component: AdivinaComponent },
+  { path: "perros", component: PerroComponent },
+  { path: "basicos-ng", component: BasicosNgComponent },
+  { path: "API-norris", component: APINorrisComponent },
+  { path: "restaurantes", component: RestaurantesComponent },
+  { path: "restaurantes/formularioRestaurante", component: FormularioRestauranteComponent },
+  { path: "restaurantesnpag", component: RestaurantesnpagComponent },
+  { path: "busqueda", component: BusquedaComponent },
+  { path: "mapa", component: MapaComponent},
+  { path: "**", redirectTo: "/"}
 ];
+  //{ path: "**", redirectTo: "restaurantes", pathMatch: "full"}
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // useHash es para que no de error al recargar la página, debido a la almohadilla que se añade al final en la URL.
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
